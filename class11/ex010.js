074b5a4e184f374c1fdd1581a5eac5da22a2978a
João Victor Mendes <j.victor8@hotmail.com>
2022-11-03 16:27:40 -0300

    updates

---
+++ class11/ex010.js
@@ -0,0 +1,5 @@
+var spd = 60.5
+
+if(spd > 60) {
+    console.log(`your speed is ${spd}kmph, you've gotten a ticket because of being over 60kmph!`)
+} 
---
+++ class11/ex011.js
@@ -0,0 +1,7 @@
+var spd = 60.5
+
+if(spd > 60) {
+    console.log(`your speed is ${spd}kmph, you've gotten a ticket because of being over 60kmph!`)
+} else {
+    console.log(`your speed is ${spd}kmph, you're ok to go!`)
+}
