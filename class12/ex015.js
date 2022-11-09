e70552c1d0489cd02ba3e24c8009397bd8b8add3
João Victor Mendes <j.victor8@hotmail.com>
2022-11-09 15:46:57 -0300

    Update ex015.js

--- class12/ex015.js
+++ class12/ex015.js
@@ -1,10 +1,10 @@
 var hour = 22  
 if (hour < 12) {
-    console.log('im only sleeping, tutu tu turu')
+    console.log('im oonly sleeeping, tutu tu turu')
 } else if (hour > 21) {
-    console.log('good night sleep tight')
+    console.log('gooood night sleeeep tight')
 } else if (hour > 18) {                
-    console.log("i'm so tired, i haven't slept a wink")
+    console.log("i'm sooo tired, i haven't slept a wink")
 } else {
     console.log("yes im lonely, turururururu, wanna dieee, tururururu")
 }
