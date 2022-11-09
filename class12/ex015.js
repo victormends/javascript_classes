4f2752c8fb759f499e2c87c8e873c32e8d4580f3
João Victor Mendes <j.victor8@hotmail.com>
2022-11-09 15:23:57 -0300

    Create ex015.js

---
+++ class12/ex015.js
@@ -0,0 +1,14 @@
+var hour = 22  
+if (hour < 12) {
+    console.log('im only sleeping, tutu tu turu')
+}   else {
+        if (hour > 21) {
+            console.log('good night sleep tight')
+        } else {
+            if (hour > 18) {                
+                console.log("i'm so tired, i haven't slept a wink")
+            } else {
+                console.log("yes im lonely, turururururu, wanna dieee, tururururu")
+            }
+        }
+    } 
