416f021a4d85da191c5f5a610aeb0db73e501665
João Victor Mendes <j.victor8@hotmail.com>
2022-11-09 11:46:49 -0300

    Create ex014.js

---
+++ class12/ex014.js
@@ -0,0 +1,10 @@
+var age = 15  
+if (age < 16) {
+    console.log('why are u so young? just grow up!')
+}   else {
+        if (age < 18) {
+            console.log('still a toddler, but able to vote')
+        } else {
+            console.log('boomer')
+        }
+    } 
