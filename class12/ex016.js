0661ae27f04f873a5e7233677bd1c5cb46ebc116
João Victor Mendes <j.victor8@hotmail.com>
2022-11-10 15:02:35 -0300

    Create ex016.js

---
+++ class12/ex016.js
@@ -0,0 +1,29 @@
+var now = new Date()
+var weekDay = now.getDay()
+console.log(weekDay)
+
+switch(dayWeek) {
+    case 0:
+        console.log('sunday')
+        break
+    case 1:
+        console.log('monday')
+        break
+    case 2:
+        console.log('tuesday')
+        break
+    case 3:
+        console.log('wednessday')
+        break
+    case 4:
+        console.log('thursday')
+        break
+    case 5:
+        console.log('friday')
+        break
+    case 6:
+        console.log('saturday')
+        break
+    default:
+        console.log('[ERROR] invalid day')
+}
