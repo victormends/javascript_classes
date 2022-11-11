258027145f0a3ed3fdc4467c4f0865de2416f120
João Victor Mendes <j.victor8@hotmail.com>
2022-11-11 10:13:15 -0300

    Update script.js

--- exercises/script.js
+++ exercises/script.js
@@ -4,20 +4,23 @@ function load() {
     var img = window.document.getElementById('img')
     var pic = window.document.getElementById('pic')
     var date = new Date()
-    var hour = date.getHours() + ':' + date.getMinutes();
-
+    // var hour = date.getHours() + ':' + date.getMinutes();
+var hour=11
     if (hour < 12) {
         msg.innerHTML = `Now we are at ${hour}am`
     } else {
         msg.innerHTML = `Now we are at ${hour}pm`
     } 
 
-    if (hour < 12) {
-        img.src='pic-morning.png'
-    } else if (hour < 18) {
-        img.src='pic-afternoon.png'
+    if (hour >= 0 && hour < 12) {
+        pic.src='pic-morning.png'
+        document.body.style.background = '#d8d8d6'
+    } else if (hour >= 12 && hour <= 18) {
+        pic.src='pic-afternoon.png'
+        document.body.style.background = '#b0d4ec'
     } else {
-        img.src='pic-night.png'
+        pic.src='pic-night.png'
+        document.body.style.background = '#183235'
     }
     
 }
