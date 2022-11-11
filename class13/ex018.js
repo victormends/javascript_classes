7c446389a75d88669ad9dc1144d1e9fbdfd6bd55
João Victor Mendes <j.victor8@hotmail.com>
2022-11-11 17:36:44 -0300

    exercises

---
+++ class13/ex018.js
@@ -0,0 +1,10 @@
+var c= 1
+// while (c <= 6) {
+//     console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+//     c++
+// }
+
+do {
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    c++
+} while (c <= 6)
