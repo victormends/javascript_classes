03bdeed6917bb1cb2b1a64f424d6dfebd77d9c94
João Victor Mendes <j.victor8@hotmail.com>
2022-11-16 14:42:09 -0300

    Update obj01.js

--- class17/obj01.js
+++ class17/obj01.js
@@ -8,3 +8,9 @@ let friend1 = {name:'jon', sex:'M', weight:69.9, getFat(w=0){
 console.log(typeof friend1)
 friend1.getFat(2)
 console.log(`${friend1.name} weights ${friend1.weight}kg`)
+
+let num = 8
+
+num **= 2
+
+console.log(num)
